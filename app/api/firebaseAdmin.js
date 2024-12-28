74962ff09c238fd13a502e6021468fe3a1274d70
Nikhil Mahamuni <nikhilmahamuni250@gmail.com>
2024-12-28 14:46:53 +0530

    first commit

---
+++ app/api/firebaseAdmin.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0MPIvQWOyRrS2gjwWdPu3ugJDgdxVHtI",
+  authDomain: "expense-tracker-9c001.firebaseapp.com",
+  projectId: "expense-tracker-9c001",
+  storageBucket: "expense-tracker-9c001.firebasestorage.app",
+  messagingSenderId: "685657672119",
+  appId: "1:685657672119:web:26a35581b0ff7db14e0032"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
